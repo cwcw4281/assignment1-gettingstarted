6134c5388b2b3b520b68410c397947bed840e354
Chunchun Wu <cw4281@nyu.edu>
2024-01-26 20:13:11 -0500

    answers

--- gettingStarted.py
+++ gettingStarted.py
@@ -20,9 +20,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 7
+        answer = 3
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 1
+        answer = 3
     else: 
         raise Exception("Invalid question")
     return(answer)
@@ -31,8 +31,8 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+    debug_question = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
+    print(type(welcome_assignment_answers(debug_question)))
 
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
